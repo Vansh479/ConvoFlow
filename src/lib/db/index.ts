a8639f90d4896dba5421e170bbf1770863d5d354
Vansh479 <146353231+Vansh479@users.noreply.github.com>
2025-07-11 14:43:02 +0530

    fixed ssl issue

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,10 +2,16 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
+
 export const pool = new Pool({
-	connectionString: process.env["PG_CONNECTION_STRING"],
-	min: 1
+    connectionString: process.env["PG_CONNECTION_STRING"],
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    min: 1,
 });
+
 pool.on("connect", () => console.log("Connected to postgres"));
 pool.on("error", (e) => console.log(e.message));
+
 export const db = drizzle(pool, { schema });
